93bf50e85a541bf6a4f863aa637eb8ab20e58b82
CarolineClark <carolinejaneclark@gmail.com>
2017-06-24 00:48:58 +0100

    Added player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private CharacterController characterController;
+	private float maxSpeed = 10;
+	private float jumpSpeed = 10;
+	private Vector3 moveDirection = Vector3.zero;
+	private float gravity = 9.8F;
+
+	void Start () {
+		Debug.Log("start");
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	void Update() {
+		moveDirection.y -= gravity * Time.deltaTime;
+		moveDirection.x = Input.GetAxis("Horizontal") * maxSpeed;
+		if (characterController.isGrounded) {
+			if (Input.GetButton("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+		}
+	}
+
+	void FixedUpdate () {
+		characterController.Move(moveDirection * Time.deltaTime);
+	}
+}
